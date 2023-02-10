216e024c8f6e0fd17c163e650c29d7d8cd4fe37e
Octiii <sirocti@gmail.com>
2023-02-10 16:02:52 +0000

    Loads of stuff!

---
+++ todo/tests_views.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+
+
+class TestViews(TestCase):
+
+    def test_get_todo_list(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/todo_list.html')
+
+    # def test_get_add_item_page(self):
--- todo/views.py
+++ todo/views.py
@@ -26,7 +26,7 @@ def add_item(request):
 
 
 def edit_item(request, item_id):
-    item = get_object_or_404(Item, id=item_id )
+    item = get_object_or_404(Item, id=item_id)
     if request.method == 'POST':
         form = ItemForm(request.POST, instance=item)
         if form.is_valid():
@@ -38,11 +38,13 @@ def edit_item(request, item_id):
     }
     return render(request, 'todo/edit_item.html', context)
 
+
 def toggle_item(request, item_id):
     item = get_object_or_404(Item, id=item_id)
     item.done = not item.done 
     item.save() 
     return redirect ('get_todo_list')
+
 
 def delete_item(request, item_id):
     item = get_object_or_404(Item, id=item_id)
